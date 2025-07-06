07cf1f24fbd5ec907aaeae53604c0f78769d0712
Om Takbhate <omtakbhate007@gmail.com>
2025-07-06 20:02:44 +0530

    fix(index): remove trailing slash from CORS origin URL

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(cors({
-    origin: "https://voizout-ui.onrender.com/",
+    origin: "https://voizout-ui.onrender.com",
     credentials: true
 }))
 
